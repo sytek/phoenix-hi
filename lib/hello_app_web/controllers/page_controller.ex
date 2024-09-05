0d179cee8b1621e8f02f1059771ed83348532803
Anibal Rodriguez <sytek1337@gmail.com>
2024-09-04 23:22:49 -0500

    Initialize repository

---
+++ lib/hello_app_web/controllers/page_controller.ex
@@ -0,0 +1,13 @@
+defmodule HelloAppWeb.PageController do
+  use HelloAppWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def hello(conn, _params) do
+    html(conn, "Ciao mondo!")
+  end
+end
